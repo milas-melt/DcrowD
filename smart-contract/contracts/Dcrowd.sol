d84a288c299afa0d2c9a9b2f0a3fc95b7d56ede8
Niceural <nicolas.bayle20@imperial.ac.uk>
2022-10-22 18:49:58 +0100

    interface finished ish

--- smart-contract/contracts/Dcrowd.sol
+++ smart-contract/contracts/Dcrowd.sol
@@ -2,26 +2,106 @@
 pragma solidity ^0.8.0;
 
 import "./interfaces/IDcrowd.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract Dcrowd is IDcrowd {
-    uint public projectCounter;
+contract Dcrowd is IDcrowd, ReentrancyGuard {
+    //----------------------------------------------------- storage
 
-    mapping(uint => ProjectInfo) public projectInfos;
+    uint private _projectCounter;
+
+    // project ID -> project info
+    mapping(uint => ProjectInfo) private _projectInfos;
+
+    // funder address -> project ID -> amount funded
+    mapping(address => mapping(uint => uint)) private _fundings;
+
+    //----------------------------------------------------- misc functions
 
     constructor() {
-        projectCounter = 0;
+        _projectCounter = 0;
     }
 
-    function createProject(uint expires, string memory uri) external override {
-        if (expires < block.timestamp) revert Dcrowd_InvalidExpires(expires);
-        uint projectId = projectCounter++;
+    //----------------------------------------------------- project functions
 
-        projectInfos[projectId] = ProjectInfo({
+    function createProject(uint64 expires, uint goal, string calldata uri) external payable override returns (uint) {
+        // expires valid
+        if (expires < block.timestamp || block.timestamp + 100 days < expires)
+            revert Dcrowd_InvalidExpires(expires);
+
+        // store project
+        uint projectId = _projectCounter++;
+        _projectInfos[projectId] = ProjectInfo({
             creator: msg.sender,
-            balance: 0,
             expires: expires,
             funded: false,
+            goal: goal,
+            balance: msg.value,
             uri: uri
         });
+
+        emit ProjectCreated(projectId, msg.sender, expires, goal, uri);
+        return projectId;
+    }
+
+    function fundProject(uint projectId) external payable override {
+        // ETH is sent
+        if (msg.value == 0) revert();
+
+        ProjectInfo memory project = _projectInfos[projectId];
+        // project exists
+        if (project.creator == address(0))
+            revert Dcrowd_ProjectNotExists(projectId);
+        // project not fully funded
+        if (project.goal <= project.balance || project.funded)
+            revert Dcrowd_ProjectAlreadyFunded(projectId);
+        if (project.expires < block.timestamp)
+            revert Dcrowd_ProjectFundingExpired(projectId);
+
+        _projectInfos[projectId].balance += msg.value;
+        _fundings[msg.sender][projectId] += msg.value;
+        emit ProjectFunded(projectId, msg.sender, msg.value);
+    }
+
+    function collectFunds(uint projectId) external override nonReentrant {
+        ProjectInfo memory project = _projectInfos[projectId];
+        // sender is creator
+        if (msg.sender != project.creator) revert Dcrowd_NotProjectCreator(msg.sender);
+        // project is fully funded
+        if (project.balance < project.goal) revert Dcrowd_ProjectNotFunded(projectId);
+        // funds have not been already collected
+        if (project.funded) revert Dcrowd_ProjectAlreadyFunded(projectId);
+
+        // update storage
+        _projectInfos[projectId].funded = true;
+
+        // transfer funds
+        (bool success, ) = project.creator.call{value: project.balance, gas: 2300}("");
+        if (!success) revert Dcrowd_TransferFailed(project.creator, project.balance);
+        emit FundsCollected(projectId, project.creator, project.balance);
+    }
+
+    function cancelFunding(uint projectId, uint amount) external override {
+        ProjectInfo memory project = _projectInfos[projectId];
+        uint funding = _fundings[msg.sender][projectId];
+        // project exists
+        if (project.creator == address(0)) revert Dcrowd_ProjectNotExists(projectId);
+        // project not funded
+        if (project.funded) revert Dcrowd_ProjectAlreadyFunded(projectId);
+        // has funded
+        if (funding == 0) revert();
+
+        // transfer funds
+        (bool success, ) = msg.sender.call{value: amount, gas: 2300}("");
+        if (!success) revert Dcrowd_TransferFailed(msg.sender, amount);
+    }
+
+    //----------------------------------------------------- accessor functions
+
+    function projectCounter() external view override returns (uint) {
+        return _projectCounter;
+    }
+
+    function getProjectInfo(uint projectId) external view override returns (ProjectInfo memory) {
+        return _projectInfos[projectId];
     }
 }
--- smart-contract/contracts/interfaces/IDcrowd.sol
+++ smart-contract/contracts/interfaces/IDcrowd.sol
@@ -1,31 +1,53 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-error Dcrowd_InvalidExpires(uint deadline);
+error Dcrowd_InvalidExpires(uint expires);
+error Dcrowd_ProjectNotExists(uint projectId);
+error Dcrowd_ProjectAlreadyFunded(uint projectId);
+error Dcrowd_NotProjectCreator(address sender);
+error Dcrowd_ProjectNotFunded(uint projectId);
+error Dcrowd_TransferFailed(address to, uint value);
+error Dcrowd_ProjectFundingExpired(uint projectId);
 
 interface IDcrowd {
     //----------------------------------------------------- structs
 
     struct ProjectInfo {
-        // sender of the `createProject` transaction
+        // creator of the project
         address creator;
+        // UNIX timestamp, given by `block.timestamp`, end of funding period
+        uint64 expires;
+        // if the funds have been transferred to the project creator
+        bool funded;
+        // project funds goal
+        uint goal;
         // current funds of the project
         uint balance;
-        // UNIX timestamp, given by `block.timestamp`, end of funding period
-        uint expires;
-        bool funded;
+        // project metadata
         string uri;
     }
 
     //----------------------------------------------------- events
 
-    event ProjectCreated(uint indexed projectId, address indexed creator, uint indexed expires, string uri);
+    event ProjectCreated(uint indexed projectId, address indexed creator, uint64 indexed expires, uint goal, string uri);
+
+    event ProjectFunded(uint indexed projectId, address indexed funder, uint amount);
+
+    event FundsCollected(uint indexed projectId, address indexed creator, uint funds);
 
     //----------------------------------------------------- project functions
 
-    function createProject(uint expires, string memory uri) external returns (uint);
+    /// @param expires UNIX timextamp, end of the funding period of the project.
+    /// @return Project ID
+    function createProject(uint64 expires, uint goal, string calldata uri) external payable returns (uint);
+
+    function collectFunds(uint projectId) external;
+
+    //----------------------------------------------------- funder functions
 
     function fundProject(uint projectId) external payable;
+
+    function cancelFunding(uint projectId, uint amount) external;
 
     //----------------------------------------------------- accessor functions
 
